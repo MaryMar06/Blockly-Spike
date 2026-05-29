642dae980e6a2f66219eebedb0590e5e202e9edc
MaryMar06 <marmarina0606@gmail.com>
2026-05-29 13:58:18 +0200

    Update toolbox.js

--- blockly-esp32-robot/blockly/toolbox.js
+++ blockly-esp32-robot/blockly/toolbox.js
@@ -53,6 +53,7 @@ export const toolboxXml = `
     <block type="logic_compare"><field name="OP">LT</field></block>
     <block type="logic_compare"><field name="OP">GT</field></block>
     <block type="sp_touch_sensor"></block>
+    </category>
 
   <!-- DYNAMISCHE KATEGORIEN: Diese werden von Blockly automatisch verwaltet -->
   <category name="VARIABLES" colour="#E53935" custom="VARIABLE"></category>
