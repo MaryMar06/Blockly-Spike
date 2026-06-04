e006a36622a021d818cdbd71b046b26529e57b17
MaryMar06 <marmarina0606@gmail.com>
2026-06-04 17:28:14 +0200

    Remove sp_touch_sensor block from SENSOREN category

--- blockly-esp32-robot/blockly/toolbox.js
+++ blockly-esp32-robot/blockly/toolbox.js
@@ -48,7 +48,6 @@ export const toolboxXml = `
 <category name="SENSOREN" colour="#00BCD4">
   <block type="sp_color_get"></block>
   <block type="sp_distance_get"></block>
-  <block type="sp_touch_sensor">
     <value name="PIN">
       <shadow type="math_number">
         <field name="NUM">4</field>
